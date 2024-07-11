ea157795fa02efbc1efeccb9787850b2c6dbb37d
SungBae <ondirtypaper@gmail.com>
2024-07-11 17:10:37 +0900

    little change

--- public/script.js
+++ public/script.js
@@ -2,7 +2,10 @@ const passwordBtn = document.getElementById('button_password');
 const passwordInput = document.getElementById('input_password');
 const list = document.getElementById('list_todo');
 let hasReadAuth = false;
-const listData = ["생활코딩 - node js.", "kt 포트포워딩 설정 통해 외부 접속 허용", "database 설치"]
+const listData = ["생활코딩 - node js.", 
+    "kt 포트포워딩 설정 통해 외부 접속 허용", 
+    "Oracle database 설치",
+    "PickUpGame ver 1.0 준비"];
 
 passwordBtn.addEventListener('click', ()=>{
     if(passwordInput.value === "1106511") {
